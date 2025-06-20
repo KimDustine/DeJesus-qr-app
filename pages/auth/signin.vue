21a93e25c470e608da1181f93f9549c91a9cc218
laica <laicagerongca20@gmail.com>
2025-06-20 17:59:22 +0800

    added new txt file

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="d-flex justify-center align-center" style="height: 100vh;">
-        <v-card max-width="500" class="pa-7"  >
+        <v-card max-width="500" class="pa-8"  >
             <h1>Login  Page</h1>
             <v-form>
                 <v-text-field 
+                outlined
                 label="Email"
                 type="Email"
+                prepend-icon="mdi-email"
                 ></v-text-field>
 
                 <v-text-field 
                 label="Password"
-                type="Passowrd"
+                outlined
+                type="password"
+                prepend-icon="mdi-lock"
                 ></v-text-field>
 
                 <v-btn 
@@ -18,7 +22,7 @@
                 type="submit"
                 class="mt-4"
                 block>Login
-                ></v-btn>
+                </v-btn>
             </v-form>
         </v-card>
     </div>
